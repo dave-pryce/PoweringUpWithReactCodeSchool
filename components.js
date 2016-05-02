49385a21de3c31a03f0038412b7cc96282de8ab0
David Pryce <dpryce@seek.com.au>
2016-05-03 08:35:19 +1000

    Initial commit.

---
+++ components.js
@@ -0,0 +1,12 @@
+class StoryApp extends React.Component {
+  render() {
+    return "<div>Hello World React</div>";
+  }
+}
+
+// ES2015
+let output = document.getElementByID('story-app');
+ReactDom.render(<StoryApp />, output);
+
+// Standard Javascript
+//ReactDom.render("<StoryApp />", document.getElementByID('story-app'));
