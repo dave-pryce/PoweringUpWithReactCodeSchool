6f83539cd455174baaaf57f258e478ad0f225ca6
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-05-11 20:07:12 +1000

    syntax change.

--- components.js
+++ components.js
@@ -1,11 +1,11 @@
 class StoryApp extends React.Component {
   render() {
-    return "<div>Hello World React</div>";
+    return <div>Hello World React</div>;
   }
 }
 
 // ES2015
-let output = document.getElementByID('story-app');
+let output = document.getElementByID("story-app");
 ReactDom.render(<StoryApp />, output);
 
 // Standard Javascript
