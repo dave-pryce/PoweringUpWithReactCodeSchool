050f7eb1162ade02243fab504de6fd01cc60536b
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-06-03 14:37:59 +1000

    added second component.

--- components.js
+++ components.js
@@ -1,15 +1,30 @@
-class StoryApp extends React.Component {
+class CommentBox extends React.Component {
   render() {
     const topics = ["Angular", "React", "Firebase", "HMTL"];
-    return <div className='test'>
-    <h3>Hello World React</h3>
+    return
+    <div>
+    <h1 className='test'>Hello World React</h1>
     <ul>
     {topics.map( topic => <li>{topic}</li>)}
     </ul>
+    <h3>Comments</h3>
+      <Comment author="DP" body="Yeah"/>
+      <Comment author="AB" body="Oh Yeah"/>
+    </div>;
+  }
+}
+
+
+class Comment extends React.Component {
+  render() {
+   return
+    <div>
+    <p className='body'>{this.props.author}</p>
+    <p className='body'>{this.props.body}</p>
     </div>;
   }
 }
 
 // ES2015
-let output = document.getElementById("story-app");
-ReactDOM.render(<StoryApp />, output);
+let output = document.getElementById("comment-box");
+ReactDOM.render(<CommentBox />, output);
