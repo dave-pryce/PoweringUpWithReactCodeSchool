3675d0f796b6e2aef43bb1ed472809de782cd884
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-06-04 15:42:00 +1000

    working need () return.

--- components.js
+++ components.js
@@ -1,7 +1,8 @@
 class CommentBox extends React.Component {
   render() {
     const topics = ["Angular", "React", "Firebase", "HMTL"];
-    return
+    return(
+
     <div>
     <h1 className='test'>Hello World React</h1>
     <ul>
@@ -10,18 +11,20 @@ class CommentBox extends React.Component {
     <h3>Comments</h3>
       <Comment author="DP" body="Yeah"/>
       <Comment author="AB" body="Oh Yeah"/>
-    </div>;
+    </div>
+  );
+
   }
 }
 
 
 class Comment extends React.Component {
   render() {
-   return
-    <div>
-    <p className='body'>{this.props.author}</p>
-    <p className='body'>{this.props.body}</p>
-    </div>;
+   return(
+  <div>
+  <p className='body'>{this.props.author} {this.props.body}</p>
+  </div>
+  );
   }
 }
 
