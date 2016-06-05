c09ea1fd153839f18e1f26d8c9605acc6ab0b489
David Pryce <dpryce@seek.com.au>
2016-06-06 09:56:28 +1000

    working with dynamic arguments.

--- components.js
+++ components.js
@@ -1,19 +1,55 @@
+
+// comment Box componenet
 class CommentBox extends React.Component {
   render() {
+    const comments = this._getComments();
     return(
     <div className="comment-box">
     <Header />
-    <h3>Comments</h3>
-      <Comment author="Dan Pringle" body="Angular is cool"/>
-      <Comment author="Jonny Appleseed" body="React is awesome"/>
-      <Comment author="Mandy Moon" body="Firebase oh year"/>
+      <div className ="comment-list">
+      <h4>{comments.length} Comments</h4>
+      {comments}
+      </div>
     </div>
   );
+  }
 
+// get comments _ for custom
+  _getComments(){
+    const commentList = [
+      {id:1, author: "Dan Pringle", body: "Angular is cool" },
+      {id:2, author: "Jonny Appleseed", body: "React is awesome"},
+      {id:3, author: "Mandy Moon", body:"Firebase oh year"},
+      {id:4, author: "Sammy Smith", body:"Components Rock"}
+    ];
+
+    return commentList.map((comment) => {
+      return (
+        <Comment
+        author={comment.author}
+        body={comment.body}
+        key={comment.id}
+        />
+      );
+    });
+  }
+
+}
+
+
+// Comment Component
+class Comment extends React.Component {
+  render() {
+   return(
+  <div>
+  <p className='comment'>{this.props.author} says: {this.props.body}</p>
+  </div>
+  );
   }
 }
 
 
+// Header Components
 class Header extends React.Component {
   render() {
     const topics = ["Angular", "React", "Firebase"];
@@ -30,15 +66,7 @@ class Header extends React.Component {
 }
 
 
-class Comment extends React.Component {
-  render() {
-   return(
-  <div>
-  <p className='comment'>{this.props.author} says: {this.props.body}</p>
-  </div>
-  );
-  }
-}
+
 
 // ES2015
 let output = document.getElementById("comment-box");
